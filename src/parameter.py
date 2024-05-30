47f4467f056fa6c3739b1ce55da48530434b3d97
FrankOu2001 <wangjunyue2001@gmail.com>
2024-05-30 22:36:06 +0800

    Add some parameter.

--- src/parameter.py
+++ src/parameter.py
@@ -1,8 +1,20 @@
 from math import log2, ceil
 
+# Instruction Info
+INST_OFFSET_BITS = 1
+PREDICT_WIDTH_OFFSET_BITS = 4
+PREDICT_WIDTH_BYTES = 32
+
+# TageSC Parameters
 N_ROWS = 4096
 BT_SIZE = 2048
 NUM_BR = 2
 TAGE_CTR_BITS = 3
 UNSHUFFLE_BIT_WIDTH = ceil(log2(NUM_BR))
 N_ROWS_PER_BR = N_ROWS / NUM_BR
+
+# File
+RESET_VECTOR = 0x80000000
+
+# Global History
+GLOBAL_HISTORY_LEN = 256
--- src/util/__init__.py
+++ src/util/__init__.py
@@ -13,7 +13,7 @@ def get_lgc_br_idx(unhashed_idx: int, br_pidx: int) -> int:
     :return: unhashed_idx的低UNSHUFFLE_BIT_WIDTH位与br_pidx的异或
     """
     # unshuffle_bits = unhasshed_idx & (1 << unshuffled_bit_width) - 1
-    return (unhashed_idx & 1) ^ br_pidx
+    return (unhashed_idx & UNSHUFFLE_BIT_WIDTH) ^ br_pidx
 
 
 def get_phy_br_idx(unhashed_idx: int, br_lidx: int) -> int:
@@ -21,4 +21,4 @@ def get_phy_br_idx(unhashed_idx: int, br_lidx: int) -> int:
     :param br_lidx: 预测块中指令槽的逻辑地址
     :return: unhashed_idx的低UNSHUFFLE_BIT_WIDTH位与br_lidx低log2(NUM_BR)位的异或
     """
-    return (unhashed_idx & 1) ^ (br_lidx & ((1 << ceil(log2(NUM_BR))) - 1))  # unhashed_idx&1 ^ br_lidx&1
+    return (unhashed_idx & 1) ^ (br_lidx & UNSHUFFLE_BIT_WIDTH)  # unhashed_idx&1 ^ br_lidx&1
