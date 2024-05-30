9dd47bdc99e2535622be0314584114b412157f19
FrankOu2001 <wangjunyue2001@gmail.com>
2024-05-30 22:37:41 +0800

    Parameterize the relevant code.

--- src/models/bundle.py
+++ src/models/bundle.py
@@ -1,8 +1,10 @@
 from mlvp import Bundle
+from parameter import *
 
-__all__ = ["PredictorCtrlBundle", "PipelineCtrlBundle", "BranchPredictionReq", "BranchPredictionResp", "UpdateBundle"]
+__all__ = ["EnableCtrlBundle", "PipelineCtrlBundle", "BranchPredictionReq", "BranchPredictionResp", "UpdateBundle"]
 
-class PredictorCtrlBundle(Bundle):
+
+class EnableCtrlBundle(Bundle):
     """
     @DynamicAttrs
     io_ctrl_
@@ -13,23 +15,11 @@ class PredictorCtrlBundle(Bundle):
 class PipelineCtrlBundle(Bundle):
     """
     @DynamicAttrs
-    io_s{x}_fire, io_s{x}_valid
+    io_s{x}_fire
     """
     signals_list = [
         # ctrl of s{i} stage, 0->BPU, 1->Tage, 3->SC
         *[f"s{i}_fire_{j}" for i in range(4) for j in range(4)],
-        *[f"s{i}_ready" for i in range(1, 4)]
-    ]
-
-
-class FullBranchPredictionBundle(Bundle):
-    signals_list = [
-        "hit",
-        *[f"{type}_{i}" for i in range(2) for type in ["slot_valids", "targets", "offsets", "br_taken_mask"]],
-        "fallThroughAddr", "fallThroughErr",
-        "is_jal", "is_jalr", "is_call", "is_ret", "is_br_sharing",
-        "last_may_be_rvi_call",
-        "jalr_target"
     ]
 
 
@@ -37,21 +27,45 @@ class FoldedHistoryBundle(Bundle):
     signals_list = [f"hist_{i}_folded_hist" for i in range(18)]
 
 
+class FTBSlotBundle(Bundle):
+    signals_list = ["offset", "lower", "tarStat", "sharing", "valid"]
+
+
 class FTBEntryBundle(Bundle):
     signals_list = [
-        f"{type}_{field}" for field in ["offset", "lower", "tarStat", "valid"] for type in ["brSlots_0", "tailSlot"]
+        "valid", "pftAddr", "carry",
+        "isCall", "isRet", "isJalr", "last_may_be_rvi_call",
+        *[f"always_taken_{i}" for i in range(NUM_BR)]
+    ]
+    sub_bundles = [
+        ("brSlots_0", lambda dut: FTBSlotBundle.from_prefix(dut, "brSlots_0_")),
+        ("tailSlot", lambda dut: FTBSlotBundle.from_prefix(dut, "tailSlot_"))
+    ]
+
+
+class FullBranchPredictionBundle(Bundle):
+    signals_list = [
+        "hit",
+        *[f"{type}_{i}" for i in range(NUM_BR) for type in ["slot_valids", "targets", "offsets", "br_taken_mask"]],
+        "fallThroughAddr", "fallThroughErr",
+        "is_jal", "is_jalr", "is_call", "is_ret", "is_br_sharing",
+        "last_may_be_rvi_call",
+        "jalr_target"
     ]
 
 
 class BranchPredictionBundle(Bundle):
     signals_list = ["pc", "valid", "hasRedirect", "ftq_idx"]
     sub_bundles = [
-        ("full_pred", lambda dut: FullBranchPredictionBundle.from_regex(dut, r"full_pred_\d_(.*)"))
+        (f"full_pred", lambda dut: FullBranchPredictionBundle.from_regex(dut, r"full_pred_\d_(.*)"))
     ]
 
 
 class BranchPredictionReq(Bundle):
-    """io_in_"""
+    """
+    @DynamicAttrs
+    io_in_
+    """
     signals_list = [f"bits_s0_pc_{i}" for i in range(4)]
     sub_bundles = [
         # TageTables' folded histories
@@ -68,15 +82,15 @@ class BranchPredictionResp(Bundle):
     """
     signals_list = ["last_stage_meta"]
     sub_bundles = [
-        (f"s{i}", lambda dut: BranchPredictionBundle.from_prefix(dut, "s{i}_")) for i in range(1, 4)
+        (f"s{i}", lambda dut: BranchPredictionBundle.from_prefix(dut, f"s{i}_")) for i in range(2, 4)
     ]
 
 
 class BranchPredictionUpdate(Bundle):
     signals_list = [
         "pc", "meta", "old_entry",
-        *[f"br_taken_mask_{i}" for i in range(2)],
-        *[f"mispred_mask_{i}" for i in range(3)],
+        *[f"br_taken_mask_{i}" for i in range(NUM_BR)],
+        *[f"mispred_mask_{i}" for i in range(NUM_BR + 1)],
         "jmp_taken", "full_target",
         "cfi_idx"
     ]
@@ -92,4 +106,4 @@ class UpdateBundle(Bundle):
     io_update_
     """
     signals_list = ["valid"]
-    sub_bundles = [("bits", lambda dut: BranchPredictionUpdate.from_prefix(dut, "bits"))]
+    sub_bundles = [("bits", lambda dut: BranchPredictionUpdate.from_prefix(dut, "bits_"))]
